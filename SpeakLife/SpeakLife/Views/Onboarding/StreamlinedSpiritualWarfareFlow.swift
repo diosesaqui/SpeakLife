bd7c82fa37da975d27c659a6a3f712ea584c0550
SpeakLife Agent <speaklife@dev.local>
2026-03-18 11:45:57 +0000

    Revert "feat: add rating prompt back to onboarding flow"
    This reverts commit 02ca42f3d8414d94f67b058947c4c7e346bee91c.

--- SpeakLife/SpeakLife/Views/Onboarding/StreamlinedSpiritualWarfareFlow.swift
+++ SpeakLife/SpeakLife/Views/Onboarding/StreamlinedSpiritualWarfareFlow.swift
@@ -22,7 +22,6 @@ enum StreamlinedSpiritualTab: Int {
     case prePaywallClose = 7      // Start Increasing Today
     case subscription = 8
     case notification = 9
-    case rating = 10
 }
 
 // MARK: - Main View
@@ -105,12 +104,6 @@ struct StreamlinedSpiritualWarfareFlow: View {
                     askNotificationPermission()
                 }
                 .tag(StreamlinedSpiritualTab.notification)
-
-                // Slide 11: Rating
-                RatingView(size: geometry.size) {
-                    advance()
-                }
-                .tag(StreamlinedSpiritualTab.rating)
             }
             .frame(width: geometry.size.width)
             .ignoresSafeArea()
@@ -145,8 +138,6 @@ struct StreamlinedSpiritualWarfareFlow: View {
             case .subscription:
                 selection = .notification
             case .notification:
-                selection = .rating
-            case .rating:
                 completeOnboarding()
             }
         }
