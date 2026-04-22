c71878ed581271e5909232e89f0e2d3b40a195c8
diosesaqui <speaklife@diosesaqui.com>
2026-04-21 23:10:48 -0400

    prod 4.6

--- SpeakLife/SpeakLife/Views/HomeView.swift
+++ SpeakLife/SpeakLife/Views/HomeView.swift
@@ -379,6 +379,7 @@ struct HomeView: View {
             .tabItem {
                 Image(systemName: "quote.bubble.fill")
                     .renderingMode(.original)
+                Text("Home")
                 
             }
     }
@@ -398,6 +399,7 @@ struct HomeView: View {
                 if #available(iOS 17, *) {
                     Image(systemName: "waveform")
                         .renderingMode(.original)
+                    Text("Audio")
                 } else {
                     Image(systemName: "waveform")
                         .renderingMode(.original)
@@ -412,6 +414,7 @@ struct HomeView: View {
             .tabItem {
                 Image(systemName: "plus.bubble.fill")
                     .renderingMode(.original)
+                Text("Yours")
             }
     }
     
@@ -468,6 +471,7 @@ struct HomeView: View {
                 if #available(iOS 17, *) {
                     Image(systemName: "book.closed.fill")
                         .renderingMode(.original)
+                    Text("Bible")
                 } else {
                     Image(systemName: "person.2.fill")
                         .renderingMode(.original)
@@ -481,6 +485,7 @@ struct HomeView: View {
             .tabItem {
                 Image(systemName: "line.3.horizontal")
                     .renderingMode(.original)
+                Text("Profile")
             }
     }
     
